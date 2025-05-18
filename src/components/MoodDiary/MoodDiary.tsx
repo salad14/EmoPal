fdb57d3fd136025dda8a7b5be1a869e28a7a7583
salad14 <1791248631@qq.com>
2025-05-18 14:45:34 +0800

    修改天气显示

--- src/components/MoodDiary/MoodDiary.tsx
+++ src/components/MoodDiary/MoodDiary.tsx
@@ -241,7 +241,7 @@ const MoodDiary: React.FC = () => {
           {weatherError && <span style={{ color: 'red' }}>{weatherError}</span>}
           {weather && !isLoadingWeather && !weatherError && (
             <>
-              {weather.icon && <img src={weather.icon} alt={weather.description} />}
+              {weather.icon && <img src={`https:${weather.icon}`} alt={weather.description} />}
               <span>{weather.city} {weather.description}, {weather.temp}°C (体感 {weather.feelslike}°C)</span>
             </>
           )}
